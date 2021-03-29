9b3539acabdea21ab43b053f3b5312884c004f06
Dari Alves <darieltondf@gmail.com>
2021-03-29 18:00:24 -0300

    criando hook

--- chapter-II/primeira-aplicacao-web-reactjs/dtmoney/src/hooks/useTransactions.tsx
+++ chapter-II/primeira-aplicacao-web-reactjs/dtmoney/src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useEffect, useState } from 'react';
-import { api } from './services/api';
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import { api } from '../services/api';
 
 
 interface Transaction {
@@ -23,7 +23,7 @@ interface TransactionsContextData {
 } 
 
 
-export const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData);
+const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData);
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
     const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -54,4 +54,10 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
             {children}
         </TransactionsContext.Provider>
     );
+}
+
+export function useTransactions() {
+    const context = useContext(TransactionsContext);
+
+    return context;
 }
