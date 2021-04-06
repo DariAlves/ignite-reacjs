64ea1fca0c9df6dbb53f4abd25a6f59db8e43e69
Dari Alves <darieltondf@gmail.com>
2021-04-06 16:52:20 -0300

    titulo dinamico por pagina

--- chapter-III/ignews/src/pages/index.tsx
+++ chapter-III/ignews/src/pages/index.tsx
@@ -1,9 +1,15 @@
 import styled from '../styles/home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
   return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
       <h1 className={styled.title}>
         Hello World
       </h1>
+    </>
   )
 }
