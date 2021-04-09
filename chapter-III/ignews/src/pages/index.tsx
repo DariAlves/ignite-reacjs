cf4f4a187ef290092d165b067ab7088eeaeef0fa
Dari Alves <darieltondf@gmail.com>
2021-04-09 18:22:01 -0300

    consumir api do stripe (ssr)

--- chapter-III/ignews/src/pages/index.tsx
+++ chapter-III/ignews/src/pages/index.tsx
@@ -3,8 +3,16 @@ import { GetServerSideProps } from 'next';
 import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
+import { stripe } from '../services/stripe';
 
-export default function Home(props) {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -17,10 +25,10 @@ export default function Home(props) {
           <h1>New about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={ product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding"/>
@@ -30,11 +38,19 @@ export default function Home(props) {
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  console.log('teste');
+  const price = await stripe.prices.retrieve('price_1IeRGzL7u1ofIDrm7FbWzNzR')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
 
   return {
     props: {
-      nome: 'Dari'
+      product
     }
   }
 }
